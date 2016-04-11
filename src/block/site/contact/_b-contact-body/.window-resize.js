a60a367b85a574a07a920c5c7e0962e18462afdd
Alexander <alexander.zybin@gmail.com>
2016-04-11 16:27:24 +0300

    201604111627

---
+++ src/block/site/contact/_b-contact-body/.window-resize.js
@@ -0,0 +1,27 @@
+$(function(){
+	
+	var initYaMaps = function() {
+		
+		var contactMap = new ymaps.Map('contact-map-cont', {
+			center: [52.979427, 36.062993],
+			zoom: 17
+		});
+		
+		var contactMark = new ymaps.Placemark([52.979427, 36.062993], {
+			hintContent: 'Москва!',
+			balloonContent: 'Столица России'
+		});
+		
+		contactMap.geoObjects.add(contactMark);
+		
+	}
+	
+	var block = $('#contact-map-cont');
+	
+	if(block.size()) {
+		
+		ymaps.ready(initYaMaps);
+		
+	}
+	
+});
