034053b3ad299f6ed6735ac18e363d9800d0602c
Alexander <alexander.zybin@gmail.com>
2016-11-23 10:23:02 +0300

    201611231022

--- src/block/site/index/_b-smilecontent/_b-holiday-cont/.document-ready.js
+++ src/block/site/index/_b-smilecontent/_b-holiday-cont/.document-ready.js
@@ -21,6 +21,80 @@
 	};
 	$.datepicker.setDefaults($.datepicker.regional['ru']);
 	
-	$('.cal-datepicker').datepicker();
+	var cal = $('.cal-datepicker');
+	
+	var _a = $('<a/>', {
+		class : 'fecss-modal-btn fecss-modal-btn-hidden',
+		href : '#site-modal-date',
+	})
+		.appendTo($(document.body))
+	;
+	
+	cal.datepicker({
+		onSelect:function (dateText, inst) {
+			//console.log(inst);
+			/*
+			inst.currentDay
+			inst.currentMonth
+			inst.currentYear
+			*/
+			
+			var base_url = cal.attr('data-base-url') || '';
+			
+			var day = '' + inst.currentDay;
+			var month = '' + (inst.currentMonth + 1);
+			
+			if(day.length == 1) {
+				day = '0' + day;
+			}
+			if(month.length == 1) {
+				month = '0' + month;
+			}
+			
+			var getDateContentByDate;
+			
+			if(typeof window.getDateContentByDate != 'undefined') {
+				
+				getDateContentByDate = window.getDateContentByDate;
+				
+			} else {
+				
+				console.log('Undefined function getDateContentByDate!');
+				
+				getDateContentByDate = function(str) {
+					return 0;
+				};
+				
+			}
+			
+			var item_id = getDateContentByDate('' + month + day);
+			
+			$.post(base_url,
+				{
+					'action':'azbnajax_call',
+					'method':'getDateContentByDate',
+					'id':item_id,
+					'type':'plain',
+				},
+				function(data){
+					
+					$('#site-modal-date-content').html(data);
+					
+					_a.trigger('click');
+					
+					/*
+					$('a.fecss-modal-btn[href="#site-modal-date"]')
+						//.appentTo($(document.body))
+						.trigger('click')
+						//.empty()
+						//.remove()
+					;
+					*/
+					
+				}
+			);
+			
+		},
+	});
 	
 })();
